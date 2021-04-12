482b44926b0c3a348e52157f0e3bd766b61af3b9
jiminpi <jiminpiee@gmail.com>
2021-04-12 15:20:59 +0800

    Create 1597.py

---
+++ 1597.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class Node(object):
+#     def __init__(self, val=" ", left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def expTree(self, s: str) -> 'Node':
+        ops = {'+':1, '-': 1, '*':2, '/':2}
+        ops_stk = []
+        node_stk = []
+        for c in s:
+            if c.isdigit():
+                node_stk.append(Node(c))
+            elif c == '(':
+                ops_stk.append(c)
+            elif c == ')':
+                while ops_stk and ops_stk[-1] != '(':
+                    right = node_stk.pop()
+                    left = node_stk.pop()
+                    cur = Node(ops_stk.pop(), left, right)
+                    node_stk.append(cur)
+                ops_stk.pop()
+            else:
+                while ops_stk and ops_stk[-1]!='(' and ops[ops_stk[-1]] >= ops[c]:
+                    right = node_stk.pop()
+                    left = node_stk.pop()
+                    cur = Node(ops_stk.pop(), left, right)
+                    node_stk.append(cur)
+                ops_stk.append(c)
+        while ops_stk:
+            right = node_stk.pop()
+            left = node_stk.pop()
+            cur = Node(ops_stk.pop(), left, right)
+            node_stk.append(cur)
+        return node_stk[0]
